2b056f7bef71af14c7b4596cf1bc68b996e53b1b
Mithunraj1590 <mithunraj1590@gmail.com>
2025-05-26 21:14:24 +0530

    initial commit

---
+++ instrument.server.mjs
@@ -0,0 +1,14 @@
+import * as Sentry from "@sentry/react-router";
+import { nodeProfilingIntegration } from '@sentry/profiling-node';
+
+Sentry.init({
+  dsn: "https://a176fbb6031c69c7218dc0ed78de4c75@o4507644700917760.ingest.us.sentry.io/4509389125124096",
+  
+  // Adds request headers and IP for users, for more info visit:
+  // https://docs.sentry.io/platforms/javascript/guides/react-router/configuration/options/#sendDefaultPii
+  sendDefaultPii: true,
+  
+  integrations: [nodeProfilingIntegration()],
+  tracesSampleRate: 1.0, // Capture 100% of the transactions
+  profilesSampleRate: 1.0, // profile every transaction
+});
---
+++ vite.config.ts
@@ -0,0 +1,23 @@
+import { reactRouter } from "@react-router/dev/vite";
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
+import {sentryReactRouter, type SentryReactRouterBuildOptions} from "@sentry/react-router";
+
+const sentryConfig: SentryReactRouterBuildOptions = {
+  org: "gctech",
+  project: "worktowander",
+  // An auth token is required for uploading source maps.
+  authToken: "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"
+  // ...
+};
+
+export default defineConfig(config => {
+  return {
+    plugins: [tailwindcss(), tsconfigPaths(), reactRouter(), sentryReactRouter(sentryConfig, config)],
+    sentryConfig,
+    ssr: {
+      noExternal: [/@syncfusion/]
+    }
+  };
+});
